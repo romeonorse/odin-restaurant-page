2818a14ae59a4c50b8f3a70fd12072649f3c4f75
romeonorse <romeonorse@gmail.com>
2022-07-11 21:07:29 +0300

    Add images through webpack

--- src/drinksLoad.js
+++ src/drinksLoad.js
@@ -1,3 +1,7 @@
+import Beer from './img/drinks/beer.jpg';
+import Snacks from './img/drinks/snacks.jpg';
+import Games from './img/drinks/games.jpg';
+
 function drinksLoad() {
   // // Menu Items
   const menuItems = document.createElement('div');
@@ -8,27 +12,37 @@ function drinksLoad() {
   const itemOne = document.createElement('div');
   itemOne.classList.add('item');
   itemOne.innerHTML = `
-    <img src="./drinks/beer.jpg">
     <h3>Beer</h3>
     <h4>$5</h4>
   `
+  const beer = new Image();
+  beer.src = Beer;
+  itemOne.appendChild(beer);
+
   menuItems.appendChild(itemOne);
 
   const itemTwo = document.createElement('div');
   itemTwo.classList.add('item');
   itemTwo.innerHTML = `
-    <img src="./drinks/snacks.jpg">
     <h3>Snacks</h3>
     <h4>$10</h4>
   `
+  const snacks = new Image();
+  snacks.src = Snacks;
+  itemTwo.appendChild(snacks);
+
   menuItems.appendChild(itemTwo);
+
   const itemThree = document.createElement('div');
   itemThree.classList.add('item');
   itemThree.innerHTML = `
-    <img src="./drinks/games.jpg">
     <h3>Fun</h3>
     <h4>Priceless</h4>
   `
+  const games = new Image();
+  games.src = Games;
+  itemThree.appendChild(games);
+
   menuItems.appendChild(itemThree);
   // document.body.appendChild(itemOne);
   // const itemTwo = document.createElement('div');
